e18bba0f7c77abaa7f8a1fd76603a49a7acf2b48
hyunnni <hihyunjj@gmail.com>
2025-01-01 20:09:49 +0900

    Initialize folder structure and add first files

---
+++ problems/baekjoon/10093.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long a, b;
+    cin >> a >> b;
+
+    if(a == b){
+        cout << 0;
+    }
+    else if(a-b > 0){
+        cout << a-b-1 << '\n';
+        for(long long i = b+1 ; i< a ; i++){
+            cout << i << ' ';
+        }
+    }
+    else{
+        cout << b-a-1 << '\n';
+        for(long long i = a+1 ; i< b ; i++){
+            cout << i << ' ';
+        }
+    }
+}
