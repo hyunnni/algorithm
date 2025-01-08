2dea2d8d1d3493c44bf80c2ab86de2c02598016a
hyunnni <hihyunjj@gmail.com>
2025-01-08 22:41:41 +0900

    refactor(baekjoon): Enhance BFS logic and improve readability in #2178

--- problems/baekjoon/2178.py
+++ problems/baekjoon/2178.py
@@ -1,34 +1,40 @@
 import sys
 from collections import deque
 
+# 미로 크기 입력
 height, width = map(int,sys.stdin.readline().split())
-maze = []
+maze = [list(map(int, sys.stdin.readline().strip())) for _ in range(height)]
 
-for _ in range(height):
-    maze.append(list(map(int,sys.stdin.readline().strip())))
-
-def bfs(maze, start_x, start_y):
-    queue = deque([[start_y, start_x,1]])
+# BFS
+def bfs(start_x, start_y):
+    # 큐 초기화
+    queue = deque([(start_y, start_x,1)]) # 큐에 튜플 형태로 저장
+    # 방문 배열 초기화
     visited = [[False] * width for _ in range(height)]
     visited[start_y][start_x] = True
 
+    # 이동 방향(상하좌우)
     dx = [-1,1,0,0]   
     dy = [0,0,-1,1]
 
     while queue:
         cury, curx, dist = queue.popleft()
         
+        # 도착지점에 도달하면 거리 반환
         if cury == height - 1 and curx == width - 1:
-            print(dist)
-            return
+            return dist
 
+        # 상하좌우 탐색
         for i in range(4):
             nx = curx + dx[i]
             ny = cury + dy[i]
-
+            
+            # 범위 확인, 방문하지 않은 칸 처리
             if 0 <= nx < width and 0 <= ny < height:
                 if maze[ny][nx] == 1 and visited[ny][nx] == False:
                     queue.append([ny, nx, dist+1])
                     visited[ny][nx] = True
 
-bfs(maze, 0, 0)
+# 실행 및 결과 출력
+result = bfs(0, 0)
+print(result)
