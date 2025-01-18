7247d82f5a6f54b7b55b052f2ad66c816cdbe1a5
hyunnni <hihyunjj@gmail.com>
2025-01-19 01:01:07 +0900

    docs(baekjoon): Add explanatory comments to improve code readability

--- problems/baekjoon/18352.py
+++ problems/baekjoon/18352.py
@@ -1,29 +1,33 @@
 import sys
 from collections import deque
 
+# 입력 처리
 n, m, k, x = map(int, sys.stdin.readline().split())
 graph = [[]for _ in range (n+1)]
 for _ in range(m):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
 
+# BFS 
 def bfs(start):
-    distances = [-1]*(n+1)
+    distances = [-1]*(n+1)  # 모든 도시에 대한 거리 초기화 (-1: 미방문)
     queue = deque([start])
-    distances[start] = 0
+    distances[start] = 0    # 시작 도시의 거리는 0
     
     while queue:
         cur = queue.popleft()
 
         for neighbor in graph[cur]:
-            if distances[neighbor] == -1:
+            if distances[neighbor] == -1:   # 미방문 도시
                 distances[neighbor] = distances[cur] + 1
                 queue.append(neighbor)
     
     return distances
 
+# 시작 도시에서 모든 도시까지의 최단거리 계산
 distances = bfs(x)
 
+# 거리 k에 해당하는 도시 출력
 result = [i for i in range(1, n+1) if distances[i] == k]
 
 if result:
