93446d844f21db1c4157965c8591e397c99d4447
hyunnni <hihyunjj@gmail.com>
2025-01-20 23:30:18 +0900

    feat(baekjoon): Solve #2587 (구현)

---
+++ problems/baekjoon/2587.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int array[5], sum = 0, avg = 0;
+    for(int i = 0 ; i < 5 ; i++){
+        cin >> array[i];
+    }
+
+    for(int i = 0 ; i < 5 ; i++){
+        sum += array[i];
+    }
+
+    sort(array, array+5);
+    cout << sum/5 << "\n";
+    cout << array[2];
+}
