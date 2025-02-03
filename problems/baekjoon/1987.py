4d9fc9b01eb5df679150307b7ee6c83ae7206095
hyunnni <hihyunjj@gmail.com>
2025-02-03 16:16:05 +0900

    feat(baekjoon): Solve #1987 (BFS)

---
+++ problems/baekjoon/1987.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+def count_dist():
+    r, c = map(int, sys.stdin.readline().split())
+    grid = [list(sys.stdin.readline().strip()) for _ in range(r)]
+
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    def bfs():
+        max_dist = 1    
+        q = deque([(0, 0, 1, {grid[0][0]})])
+        
+        while q : 
+            cx , cy, dist, path = q.popleft()
+            max_dist = max(max_dist, dist)
+            
+            for i in range(4):
+                nx, ny = cx + dx[i], cy + dy[i]
+
+                if 0 <= nx < c and 0 <= ny < r and grid[ny][nx] not in path:
+                    q.append((nx, ny, dist + 1, path | {grid[ny][nx]}))
+                    
+        return max_dist
+    
+    return bfs()
+
+print(count_dist())
