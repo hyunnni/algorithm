0812fb780b03ce2b2a6c289e8eb6b1ab16c1c914
hyunnni <hihyunjj@gmail.com>
2025-02-03 16:21:43 +0900

    docs(#1987): Add explanatory comments

--- problems/baekjoon/1987.py
+++ problems/baekjoon/1987.py
@@ -10,7 +10,7 @@ def count_dist():
 
     def bfs():
         max_dist = 1    
-        q = deque([(0, 0, 1, {grid[0][0]})])
+        q = deque([(0, 0, 1, {grid[0][0]})])    # (x, y, 이동 거리, 지나온 알파벳 set)
         
         while q : 
             cx , cy, dist, path = q.popleft()
@@ -20,7 +20,7 @@ def count_dist():
                 nx, ny = cx + dx[i], cy + dy[i]
 
                 if 0 <= nx < c and 0 <= ny < r and grid[ny][nx] not in path:
-                    q.append((nx, ny, dist + 1, path | {grid[ny][nx]}))
+                    q.append((nx, ny, dist + 1, path | {grid[ny][nx]})) # 새로운 경로 저장
                     
         return max_dist
     
