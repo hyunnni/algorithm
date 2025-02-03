62dce4be4f6df9b2d91bb1ffafa931545deb702b
hyunnni <hihyunjj@gmail.com>
2025-02-03 13:53:04 +0900

    refactor(#4963): Replace grid modification with visited array

--- problems/baekjoon/4963.py
+++ problems/baekjoon/4963.py
@@ -1,40 +1,41 @@
 import sys
 from collections import deque
 
-def count_island(w, h, map):
-    
-    dx = [0, 0, 1, 1, 1, -1, -1, -1]
-    dy = [1, -1, -1, 0, 1, -1, 0, 1 ]
+def count_island(w, h, grid):
+    """BFS를 사용하여 섬의 개수 세는 함수"""
 
-    def bfs(startx, starty):
-        map[starty][startx] = -1
-        q = deque([(startx, starty)])
+    dx = [0, 0, 1, -1, 1, -1, 1, -1]  # 상, 하, 좌, 우, 대각선
+    dy = [1, -1, 0, 0, 1, 1, -1, -1]
+
+    visited = [[False] * w for _ in range(h)]  # 방문 여부 체크
+
+    def bfs(start):
+        """BFS를 사용하여 하나의 섬을 탐색"""
+        q = deque([start])
+        visited[start[1]][start[0]] = True  # 방문 처리
 
         while q:
             curx, cury = q.popleft()
-            
             for i in range(8):
-                nx, ny = curx + dx[i] , cury + dy[i]
+                nx, ny = curx + dx[i], cury + dy[i]
 
-                if 0 <= nx < w and 0 <= ny < h:
-                    if map[ny][nx] > 0:
-                        map[ny][nx] = -1
-                        q.append((nx, ny))
-    
-    island = 0
+                if 0 <= nx < w and 0 <= ny < h and not visited[ny][nx] and grid[ny][nx] == 1:
+                    visited[ny][nx] = True
+                    q.append((nx, ny))
 
+    island_count = 0
     for y in range(h):
         for x in range(w):
-            if map[y][x] == 1:
-                island += 1
-                bfs(x, y)
-                
-    return island
+            if grid[y][x] == 1 and not visited[y][x]:
+                island_count += 1
+                bfs((x, y))
+
+    return island_count
 
 while True:
     w, h = map(int, sys.stdin.readline().split())
     if w == 0 and h == 0:
         break
-    else:
-        map_list = [list(map(int, sys.stdin.readline().split())) for _ in range(h)]
-        print(count_island(w, h, map_list))
+
+    island_map = [list(map(int, sys.stdin.readline().split())) for _ in range(h)]
+    print(count_island(w, h, island_map))
