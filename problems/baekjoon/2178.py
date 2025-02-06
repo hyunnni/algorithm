7704f2a29b066de30aea74f6034fe809086b4f14
hyunnni <hihyunjj@gmail.com>
2025-02-06 20:10:35 +0900

    refactor(#2178): Re-solve #2178 (BFS)

--- problems/baekjoon/2178.py
+++ problems/baekjoon/2178.py
@@ -1,40 +1,26 @@
 import sys
 from collections import deque
 
-# 미로 크기 입력
-height, width = map(int,sys.stdin.readline().split())
-maze = [list(map(int, sys.stdin.readline().strip())) for _ in range(height)]
+N, M = map(int, sys.stdin.readline().split())
+maze = [list(map(int, sys.stdin.readline().strip()))for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-# BFS
-def bfs(start_x, start_y):
-    # 큐 초기화
-    queue = deque([(start_y, start_x,1)]) # 큐에 튜플 형태로 저장
-    # 방문 배열 초기화
-    visited = [[False] * width for _ in range(height)]
-    visited[start_y][start_x] = True
+def bfs():
+    q = deque([(0, 0, 1)])
+    visited[0][0] = True
 
-    # 이동 방향(상하좌우)
-    dx = [-1,1,0,0]   
-    dy = [0,0,-1,1]
-
-    while queue:
-        cury, curx, dist = queue.popleft()
-        
-        # 도착지점에 도달하면 거리 반환
-        if cury == height - 1 and curx == width - 1:
+    while q:
+        cx, cy, dist = q.popleft()
+        if cx == M - 1 and cy == N - 1:
             return dist
-
-        # 상하좌우 탐색
+        
         for i in range(4):
-            nx = curx + dx[i]
-            ny = cury + dy[i]
-            
-            # 범위 확인, 방문하지 않은 칸 처리
-            if 0 <= nx < width and 0 <= ny < height:
-                if maze[ny][nx] == 1 and visited[ny][nx] == False:
-                    queue.append([ny, nx, dist+1])
+            nx, ny = cx + dx[i], cy + dy[i]
+            if 0 <= nx < M and 0 <= ny < N:
+                if maze[ny][nx] == 1 and not visited[ny][nx]:
                     visited[ny][nx] = True
+                    q.append((nx, ny, dist + 1))
 
-# 실행 및 결과 출력
-result = bfs(0, 0)
-print(result)
+print(bfs())
