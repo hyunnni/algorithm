4442d5364bc8cfd0d26712925d8238ab536d7978
hyunnni <hihyunjj@gmail.com>
2025-02-06 20:16:58 +0900

    refactor(#2178): Remove visited array and store distances in maze

--- problems/baekjoon/2178.py
+++ problems/baekjoon/2178.py
@@ -8,19 +8,18 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 def bfs():
-    q = deque([(0, 0, 1)])
-    visited[0][0] = True
+    q = deque([(0, 0)])
 
     while q:
         cx, cy, dist = q.popleft()
-        if cx == M - 1 and cy == N - 1:
-            return dist
         
         for i in range(4):
             nx, ny = cx + dx[i], cy + dy[i]
-            if 0 <= nx < M and 0 <= ny < N:
-                if maze[ny][nx] == 1 and not visited[ny][nx]:
-                    visited[ny][nx] = True
-                    q.append((nx, ny, dist + 1))
+
+            if 0 <= nx < M and 0 <= ny < N and maze[ny][nx] == 1:
+                maze[ny][nx] = maze[cy][cx] + 1 # 최단거리 저장
+                q.append((nx, ny))
+
+    return maze[N - 1][M - 1]   # (N - 1 , M - 1) 위치의 값이 최단거리
 
 print(bfs())
