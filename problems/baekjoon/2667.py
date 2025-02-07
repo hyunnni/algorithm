75ab1db6362b255935c9e74be3128258a4bea432
hyunnni <hihyunjj@gmail.com>
2025-02-07 22:57:20 +0900

    refactor(#2667): Remove visited array and use grid for BFS tracking

--- problems/baekjoon/2667.py
+++ problems/baekjoon/2667.py
@@ -3,7 +3,6 @@ from collections import deque
 
 N = int(input())
 grid = [list(map(int, sys.stdin.readline().strip()))for _ in range(N)]
-visited = [[False]*N for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -11,7 +10,7 @@ complex_list = []
 
 def bfs(sx, sy):
     count = 1
-    visited[sy][sx] = True
+    grid[sy][sx] = 0
     q = deque([(sx, sy)])
 
     while q:
@@ -20,17 +19,16 @@ def bfs(sx, sy):
         for i in range(4):
             nx, ny = cx + dx[i], cy + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < N:
-                if not visited[ny][nx] and grid[ny][nx] == 1:
-                    q.append((nx, ny))
-                    visited[ny][nx] = True
-                    count += 1
+            if 0 <= nx < N and 0 <= ny < N and grid[ny][nx] == 1:
+                q.append((nx, ny))
+                grid[ny][nx] = 0
+                count += 1
 
     return count
 
 for y in range(N):
     for x in range(N):
-        if not visited[y][x] and grid[y][x] == 1:
+        if grid[y][x] == 1:
             complex_list.append(bfs(x, y))
 
 complex_list.sort()
