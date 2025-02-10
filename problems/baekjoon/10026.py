f380e4e64bb10038d9a8be8467725658ff844940
hyunnni <hihyunjj@gmail.com>
2025-02-10 23:41:46 +0900

    refactor(#10026): Simplify BFS by transforming 'R' to 'G'

--- problems/baekjoon/10026.py
+++ problems/baekjoon/10026.py
@@ -9,7 +9,7 @@ dy = [0, 0, -1, 1]
 ncb = 0
 cb = 0
 
-def bfs(sx, sy, blindness):
+def bfs(sx, sy):
     q = deque([(sx, sy, grid[sy][sx])])
     visited[sy][sx] = True
 
@@ -19,23 +19,27 @@ def bfs(sx, sy, blindness):
         for i in range(4):
             nx, ny = cx + dx[i], cy + dy[i]
             if 0 <= nx < N and 0 <= ny < N and not visited[ny][nx]:
-                if blindness and (color == grid[ny][nx] or (color in ['R','G']) and (grid[ny][nx] in ['R','G'])):
+               if color == grid[ny][nx]:
                     visited[ny][nx] = True
                     q.append((nx ,ny, color))
 
-                elif not blindness and color == grid[ny][nx]:
-                    visited[ny][nx] = True
-                    q.append((nx ,ny, color))
+visited = [[False]*N for _ in range(N)]
+for y in range(N):
+    for x in range(N):
+        if not visited[y][x]:
+            bfs(x, y)
+            ncb += 1
 
-for i in range(2):
-    visited = [[False]*N for _ in range(N)]
-    for y in range(N):
-        for x in range(N):
-            if not visited[y][x]:
-                bfs(x, y, i)
-                if i == 0:
-                    ncb += 1
-                else:
-                    cb += 1
+for y in range(N):
+    for x in range(N):
+        if grid[y][x] == 'R':
+            grid[y][x] = 'G'
+
+visited = [[False]*N for _ in range(N)]
+for y in range(N):
+    for x in range(N):
+        if not visited[y][x]:
+            bfs(x, y)
+            cb += 1  
 
 print(ncb, cb)
