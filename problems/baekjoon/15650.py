07b5b1f0517189232c1e4dc86a96c1729e8ce8b4
hyunnni <hihyunjj@gmail.com>
2025-02-18 21:54:28 +0900

    docs(#15650): Add explanatory comments

--- problems/baekjoon/15650.py
+++ problems/baekjoon/15650.py
@@ -4,13 +4,13 @@ N, M = map(int, sys.stdin.readline().split())
 result = []
 
 def backtrack(start, depth):
-    if depth == M:
+    if depth == M:  # M개 선택하면 출력
         print(*result)
         return
 
-    for i in range(start, N + 1):
+    for i in range(start, N + 1):   # start부터 탐색 (중복X)
         result.append(i)
-        backtrack(i + 1, depth + 1)
-        result.pop()
+        backtrack(i + 1, depth + 1) # (오름차순)
+        result.pop()    # 원상복구
 
 backtrack(1, 0)
