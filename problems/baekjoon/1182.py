c4524d976898af365907df237bb920cabd7edd9d
hyunnni <hihyunjj@gmail.com>
2025-02-19 23:16:29 +0900

    docs(#1182): Add explanatory comments

--- problems/baekjoon/1182.py
+++ problems/baekjoon/1182.py
@@ -8,16 +8,16 @@ count = 0
 def backtrack(idx, total):
     global count
 
-    if idx >= N:
+    if idx >= N:    #모든 원소 확인한 경우
         return
     
+    # 현재 원소 선택한 경우
     total += numbers[idx]
     if total == S:
-        count += 1
+        count += 1  # 조건 만족하는 경우 카운트 ++
 
-    backtrack(idx + 1, total)
-    backtrack(idx + 1, total - numbers[idx])
+    backtrack(idx + 1, total)   # 다음 원소 탐색 (현재 원소 선택한 경우)
+    backtrack(idx + 1, total - numbers[idx])    # 원상복구
 
 backtrack(0,0)
 print(count)
-    
